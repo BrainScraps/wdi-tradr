604e873ea8f5d044d34214415f02cf438430d846
Isaac Elias <nicecurelom@gmail.com>
2013-07-25 23:57:52 -0700

    the add stock refreshes the table properly

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,6 +2,7 @@ class StocksController < ApplicationController
   def index
     @user = current_user
     @stocks = current_user.stocks
+    @means = current_user.stock_means
 
   end
 
@@ -12,11 +13,17 @@ class StocksController < ApplicationController
   def create
     #current_user = @user
     @user = User.find(current_user.id)
-    @stock = @user.stocks.new(symbol: params[:symbol], quantity: params[:quntity])
+    @stock = @user.stocks.new(symbol: params[:symbol], quantity: params[:quantity].to_i)
     @stock.set_price(params[:symbol])
     @stock.save
 
     @stocks = current_user.stocks
+    @means = current_user.stock_means
+  end
+
+  def refresh_data
+
+  
   end
 
   def show
@@ -30,4 +37,22 @@ class StocksController < ApplicationController
 
   def destroy
   end
+
+  def get_price(symbol)
+
+    quote = Stockery::Quote.new
+
+    resp = quote.get_status(symbol)
+
+    resp[:price]
+
+  end
+
+  def get_profit(quantity,paid, quote)
+     (quantity * quote) - (quantity * paid)
+  end
+
+  helper_method :get_price 
+  helper_method :get_profit
+
 end
